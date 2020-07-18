6e6bb04cee1866f5bc22c6889725633bc303f26b
Lucas Alencar <lucasbalencar05@gmail.com>
2020-07-17 23:04:04 -0300

    Rest init

---
+++ src/main/java/com/lucasalencar/cursomc/resources/CategoriesResource.java
@@ -0,0 +1,16 @@
+package com.lucasalencar.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoriesResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "Rest API";
+    }
+
+}
