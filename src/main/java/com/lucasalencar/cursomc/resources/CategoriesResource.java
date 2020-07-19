3beb5af44bb458008f874f41c4f992a62f931aa6
Lucas Alencar <lucasbalencar05@gmail.com>
2020-07-19 17:48:55 -0300

    Category class

--- src/main/java/com/lucasalencar/cursomc/resources/CategoriesResource.java
+++ src/main/java/com/lucasalencar/cursomc/resources/CategoriesResource.java
@@ -1,16 +1,29 @@
 package com.lucasalencar.cursomc.resources;
 
+import com.lucasalencar.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriesResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String index() {
-        return "Rest API";
+    public List<Category> index() {
+
+        Category cat1 = new Category(1, "Informática");
+        Category cat2 = new Category(2, "Escritório");
+
+        List<Category> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 
 }
